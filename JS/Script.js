3fa604e40a2a3311d81b28c58f29f222ce1f50ce
Mehedi-irfan <mehediirfan99@gmal.com>
2023-03-02 23:19:12 +0600

    complete the shop page with single product page

--- JS/Script.js
+++ JS/Script.js
@@ -12,3 +12,20 @@ if (close) {
     nav.classList.remove("active");
   });
 }
+
+// ==========Single Page================
+let mainImg = document.getElementById("mainImg");
+let smallImg = document.getElementsByClassName("small-img");
+
+smallImg[0].onclick = function () {
+  mainImg.src = smallImg[0].src;
+};
+smallImg[1].onclick = function () {
+  mainImg.src = smallImg[1].src;
+};
+smallImg[2].onclick = function () {
+  mainImg.src = smallImg[2].src;
+};
+smallImg[3].onclick = function () {
+  mainImg.src = smallImg[3].src;
+};
